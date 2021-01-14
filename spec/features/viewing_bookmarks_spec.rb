7f5a3f94c19f04c05fa4d742992844973014c6ea
arbOP <newarbnor@hotmail.com>
2021-01-14 22:05:14 +0000

    Playing around with coding, may not need the expect(page) to have section as the database has our URLs

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -12,5 +12,6 @@ feature 'show books' do
     # expect(page).to have_content "http://www.makersacademy.com"
     # expect(page).to have_content "http://www.destroyallsoftware.com"
     # expect(page).to have_content "http://www.google.com"
+
   end
 end
